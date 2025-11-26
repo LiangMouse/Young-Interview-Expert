65159e6b1d7daec1aae12d68ea12cfe091418a33
LiangMouse <1979139861l@gmail.com>
2025-11-26 23:38:17 +0800

    feat: download Geist to prevent network error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
-const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
+const geist = localFont({
+  src: "../../public/fonts/Geist/Geist-VariableFont_wght.ttf",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "../../public/fonts/Geist_Mono/GeistMono-VariableFont_wght.ttf",
   variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
